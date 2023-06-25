52d542512bd6332b3d6d9c3b788bde2e069dc725
Gabriel Ferreiras <gabriel.ruasmil@gmail.com>
2023-06-25 18:21:35 -0300

    voltando com os estudos

---
+++ ex000/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro código em PHP</title>
+</head>
+<body>
+    
+    <h1>
+        <?php 
+            echo "Olá, Mundo! \u{1F30F}";
+        ?>
+    </h1>
+
+    <h2>
+        <?php 
+            print "Me livrei da maldição! \u{1F604}"
+        ?>
+    </h2>
+    <p>Vamos nos livrar da maldição</p>
+</body>
+</html>
